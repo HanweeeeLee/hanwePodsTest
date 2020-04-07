d3ff89da8418b8c9755b76dec49587082485a56b
hanwe lee <hwlee@raoncorp.com>
2020-04-07 17:17:33 +0900

    no message

---
+++ hanwePodsTest/hanwePodsTest/TestClass.swift
@@ -0,0 +1,15 @@
+//
+//  TestClass.swift
+//  hanwePodsTest
+//
+//  Created by hanwe lee on 2020/04/07.
+//  Copyright © 2020 hanwe lee. All rights reserved.
+//
+
+import UIKit
+
+public class TestClass: NSObject {
+    public static func testFunc() {
+        print("success!!")
+    }
+}
